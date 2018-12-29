0e0312fb02c9204385de906725994962d373f1ce
chniak <urbaniak.n78@gmail.com>
2018-12-29 19:10:59 +0100

    animate

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,10 @@ console.log("gggg");
 $(document).ready(function(){
     $('#dc').click(function(){
         $('.design-education').toggleClass("hidden","addOrRemove");
-        $('.title').toggle({
-            opacity:"+=1"
-        },1000,function(){
+    //     $('.title').toggle({
+    //         opacity:"+=1"
+    //     },1000,function(){
 
-        });
+    //     });
     });
 })
