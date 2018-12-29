ba9f486e03b58a3578030c9279081ba8f548e0f6
chniak <urbaniak.n78@gmail.com>
2018-12-29 19:26:46 +0100

    animate

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,7 @@ console.log("gggg");
 $(document).ready(function(){
     $('#dc').click(function(){
         $('.design-education').toggleClass("hidden","addOrRemove");
-    //     $('.title').toggle({
-    //         opacity:"+=1"
-    //     },1000,function(){
-
-    //     });
+        $('.title').toggle(600);
+       
     });
 })
