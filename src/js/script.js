cd7034da1067a7c5e418aeefc4ca5c5de4121cb7
chniak <urbaniak.n78@gmail.com>
2018-12-29 19:04:38 +0100

    animate

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,10 @@ console.log("gggg");
 $(document).ready(function(){
     $('#dc').click(function(){
         $('.design-education').toggleClass("hidden","addOrRemove");
-    })
+        $('.title').toggle({
+            opacity:"+=1"
+        },1000,function(){
+
+        });
+    });
 })
