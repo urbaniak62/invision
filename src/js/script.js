deab236247658eaedb43c88bdfd76073f59e8a87
chniak <urbaniak.n78@gmail.com>
2018-12-30 20:48:11 +0100

    animate

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,32 @@ $(document).ready(function(){
         $('.title').toggle(600);
        
     });
+    var flag=false;
+    var scroll;
+    $(window).scroll(function(){
+       scroll=$(window).scrollTop();
+       console.log(scroll);
+       if (scroll >57) {
+           $(".nav-left").css({
+               "position":"fixed",
+               "background":"white",
+               "z-index":"999",
+               
+           });
+           $(".nav-left ul li a").css({
+                "color":"#ff3366",
+           })
+       }
+       if (scroll<57) {
+        $(".nav-left").css({
+            "position":"absolute",
+            "background":"transparent",
+            "z-index":"999",
+            
+        });
+        $(".nav-left ul li a").css({
+             "color":"#white",
+        })
+       }
+    })
 })
